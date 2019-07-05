245617879f9f4bc25820d30eb40ae179368f2063
Phetcharat Phanthachai <phetcharatphanthachai@Phetcharats-MacBook-Pro.local>
2019-07-05 20:00:08 +0700

    add train folder again

---
+++ train/try_python/test.py
@@ -0,0 +1,9 @@
+dic = {
+    name : "bow",
+    age : 12,
+    favorite : [
+        1,2,3,4
+    ]
+}
+
+# for ()    
---
+++ train/try_react/my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import content from './component/content'
+
+function App() {
+  return (
+    <div>
+      <content />
+      <h3>this app</h3>
+    </div>
+  );
+}
+
+export default App;
